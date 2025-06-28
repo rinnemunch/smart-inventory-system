3bf89ca31018d81346b66ebff4e1a92a307cbcc5
Shaun Fulton <fulton_shaun@yahoo.com>
2025-06-28 10:18:03 -0400

    Create ItemRepository class with in-memory item list

---
+++ inventory-service/src/main/java/com/inventory/inventory_service/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.inventory.inventory_service.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.inventory.inventory_service.model.Item;
+
+public class ItemRepository {
+    private List<Item> items = new ArrayList<>();
+}
